004b16f6c43ee176a828b32e268e9e02f9900520
lokej <lokej+git@uci.edu>
2019-02-15 22:58:04 -0800

    router works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {MapComponent} from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'main',
+        component: MainComponent
+    },
+    {
+        path: 'map',
+        component: MapComponent
+    },
+    {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full'
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
